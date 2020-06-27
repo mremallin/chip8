ea2938e96804a381b2896d39aadeff7598610c22
Mike Mallin <mike@mremallin.ca>
2020-06-26 21:29:21 -0400

    Implemented 0 based opcodes

--- chip8.c
+++ chip8.c
@@ -14,7 +14,10 @@
 /* Information from https://en.wikipedia.org/wiki/CHIP-8 */
 #define PROGRAM_LOAD_ADDR       0x200
 #define STACK_END_ADDR          0xEA0
-#define STACK_BASE_ADDR         0xEFF
+/* 0xEFF is the last valid address in the stack, but because the
+ * stack stores 16-bit pointers we start at 0xEFE to not overwrite
+ * past the stack boundaries */
+#define STACK_BASE_ADDR         0xEFE
 #define DISPLAY_REFRESH_ADDR    0xF00
 #define MEMORY_SIZE             0x1000
 
@@ -29,6 +32,9 @@ static uint16_t s_i_reg;
 
 /* 16-bit program counter */
 static uint16_t s_pc;
+
+/* 16-bit stack pointer */
+static uint16_t s_stack_ptr;
 
 /* Timers */
 
@@ -55,6 +61,11 @@ chip8_interpret_op0 (uint16_t op)
         case 0x00E0:
             clear_display();
             break;
+        case 0x00EE:
+            /* Pop stack to PC */
+            s_pc = *(uint16_t *)&s_memory[s_stack_ptr];
+            s_stack_ptr += 2;
+            break;
     }
 }
 
@@ -72,4 +83,15 @@ chip8_interpret_op (uint16_t op)
     /* Dispatch opcode decoding to the right class of opcode.
      * The uppermost nibble is a fixed constant from 0-F. */
     s_opcode_decoder[op_class](op);
+}
+
+void
+chip8_init (void)
+{
+    memset(s_memory, 0, sizeof(s_memory));
+    memset(s_v_regs, 0, sizeof(s_v_regs));
+    s_i_reg = 0;
+    s_pc = PROGRAM_LOAD_ADDR;
+    s_stack_ptr = STACK_BASE_ADDR;
+    memset(s_vram, 0, sizeof(s_vram));
 }
--- chip8_test.c
+++ chip8_test.c
@@ -6,7 +6,9 @@
 #include "chip8.c"
 
 /* A test case that does nothing and succeeds. */
-static void opc_00E0(void **state) {
+static void
+opc_00E0(void **state)
+{
 	static uint8_t s_zero[BITS2BYTES(DISPLAY_WIDTH*DISPLAY_HEIGHT)] = {0};
 
 	memset(s_vram, 0xFE, sizeof(s_vram));
@@ -14,10 +16,29 @@ static void opc_00E0(void **state) {
 	assert_memory_equal(s_vram, s_zero, sizeof(s_vram));
 }
 
+static void
+opc_00EE(void **state)
+{
+	/* Returns from a subroutine */
+	s_stack_ptr = 0xE00;
+	*(uint16_t *)&s_memory[s_stack_ptr] = 0xDEAD;
+	chip8_interpret_op(0x0EE);
+	assert_int_equal(s_pc, 0xDEAD);
+	assert_int_equal(s_stack_ptr, 0xE02);
+}
+
+static int
+chip8_test_init (void **state)
+{
+	chip8_init();
+	return 0;
+}
+
 int
 main(int argc, char *argv[]) {
     const struct CMUnitTest chip8_opc_0[] = {
         cmocka_unit_test(opc_00E0),
+        cmocka_unit_test(opc_00EE),
     };
-    return cmocka_run_group_tests(chip8_opc_0, NULL, NULL);
+    return cmocka_run_group_tests(chip8_opc_0, chip8_test_init, NULL);
 }
